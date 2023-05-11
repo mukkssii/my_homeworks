c0f406f3607e9fca48b4f99d3de83b688b2f0dd1
“mukkssii” <kapitanbukash@gmail.com>
2023-05-11 08:28:12 +0200

    I did homework 20

---
+++ hw_20.py
@@ -0,0 +1,17 @@
+ukrainian_presidents = ['Леонід Кравчук',
+                        'Леонід Кучма',
+                        'Віктор Ющенко',
+                        'Віктор Янукович',
+                        'Олександр Турчинов',
+                        'Петро Порошенко',
+                        'Володимир Зеленський',
+                        ]
+
+presidents = (president for president in ukrainian_presidents)
+print(next(presidents))
+print(next(presidents))
+print(next(presidents))
+print(next(presidents))
+print(next(presidents))
+print(next(presidents))
+print(next(presidents))
